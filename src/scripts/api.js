75c0ad8ef23bfde54b7137d6836c6cace45e6809
Daniel Krusch <daniel.krusch@gmail.com>
2019-07-03 11:31:12 -0500

    added event card functionality

--- src/scripts/api.js
+++ src/scripts/api.js
@@ -1,8 +1,40 @@
+import { Action } from "./action";
+
 console.log("API loaded")
 
 export const API = {
   getValues() {
     return fetch("http://localhost:8088/users")
       .then(response => response.json());
+  },
+
+  getEvents() {
+    return fetch("http://localhost:8088/events")
+      .then(response => response.json());
+  },
+
+  deleteCard (name, id) {
+    fetch(`http://localhost:8088/${name}/${id}`,
+    {
+      method: "DELETE",
+      headers: {
+          "Content-Type": "application/json"
+      }
+    })
+    .then(res => res.json())
+    .then(() => Action.addEvent())
+  },
+
+  // Updates an entry with the passed id with the new values, then displays all the entries to the dom
+  editCard (name, id, card)
+  {
+    fetch(`http://localhost:8088/${name}/${id}`, {
+    method: "PUT",
+    headers: {
+        "Content-Type": "application/json"
+    },
+    body: JSON.stringify(card)
+    })
+    .then(() => Action.addEvent())
   }
 }
